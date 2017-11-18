126fd60faa4c0454b8231abd918b2e71aa6f7e32
krishna kumar <krishnau2@gmail.com>
2017-11-18 23:10:17 +0530

    Added account model and STI for Bank accounts and Wishlist accounts

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    @bank_accounts = BankAccount.all
+    @wishlist_accounts = WishlistAccount.all
+  end
+end
---
+++ app/models/wishlist_account.rb
@@ -0,0 +1,4 @@
+class WishlistAccount < Account
+  
+  
+end
