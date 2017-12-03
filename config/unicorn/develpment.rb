d77c92df4f68b446f32c48f1bacd6243b0cac5e1
krishna kumar <krishnau2@gmail.com>
2017-12-03 15:04:10 +0530

    Minor corrections in unicorn conf files.

--- config/unicorn/develpment.rb
+++ config/unicorn/develpment.rb
@@ -11,31 +11,31 @@ timeout 30
 preload_app true
  
 before_exec do |server|
- ENV[‘BUNDLE_GEMFILE’] = "#{root}/Gemfile"
+  ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
 end
  
 before_fork do |server, worker|
  # the following is highly recomended for Rails + “preload_app true”
  # as there’s no need for the master process to hold a connection
- if defined?(ActiveRecord::Base)
- ActiveRecord::Base.connection.disconnect!
- end
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
  
  # Before forking, kill the master process that belongs to the .oldbin PID.
  # This enables 0 downtime deploys.
- old_pid = "#{root}/tmp/pids/unicorn.pid.oldbin"
- if File.exists?(old_pid) && server.pid != old_pid
- begin
- Process.kill("QUIT", File.read(old_pid).to_i)
- rescue Errno::ENOENT, Errno::ESRCH
- # someone else did our job for us
- end
- end
+  old_pid = "#{root}/tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+      rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
  
 after_fork do |server, worker|
  # the following is *required* for Rails + “preload_app true”,
- if defined?(ActiveRecord::Base)
- ActiveRecord::Base.establish_connection
- end
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
 end
