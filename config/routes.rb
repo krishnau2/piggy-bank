b9807fe80fa7473ddbef91769078bed25a8d7445
krishna kumar <krishnau2@gmail.com>
2017-12-05 09:20:36 +0530

    Added chart of accounts page to manage accounts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :wishlist_accounts
   resources :bank_accounts
+  get '/chart_of_accounts', to: 'chart_of_accounts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 end
