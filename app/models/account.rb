303ca544cc1d128f01957529b1e9ee61d2dda78b
krishna kumar <krishnau2@gmail.com>
2018-01-29 02:04:25 +0530

    Added slug creation for accounts while creating account entry.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,13 @@
 class Account < ApplicationRecord
   has_many :bank_transactions
+  before_create :set_slug
 
   def amount
     return nil
   end
+
+  private
+    def set_slug
+      self.slug = self.name.parameterize.underscore
+    end
 end
